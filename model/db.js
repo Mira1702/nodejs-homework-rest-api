9f6c4b745a44223967c45598781d859a928e0382
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-05-16 18:24:10 +0300

    выполняет соединение с базой данных

--- model/db.js
+++ model/db.js
@@ -1,9 +1,25 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+// const low = require("lowdb");
+// const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("./model/contacts.json");
-const db = low(adapter);
+// const adapter = new FileSync("./model/contacts.json");
+// const db = low(adapter);
 
-db.defaults({ contacts: [] }).write();
+// db.defaults({ contacts: [] }).write();
+
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uriDB = process.env.URI_DB;
+const db = MongoClient.connect(uriDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 5,
+});
+
+process.on("SIGINT", async () => {
+  const client = await db;
+  client.close();
+  console.log("Disconnect MongoDB");
+  process.exit();
+});
 
 module.exports = db;
