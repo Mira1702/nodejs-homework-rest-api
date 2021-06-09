b67ecc791ac26e9b62a13e6c1615815bc67aa054
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-06-09 22:17:32 +0300

    выполняет шаг 4

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 const boolParser = require("express-query-boolean");
 const helmet = require("helmet");
-const limiter = require("./helpers/limiter");
+// const limiter = require("./helpers/limiter");
 
 const { HttpCode } = require("./helpers/constans");
 
@@ -14,11 +15,15 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+require("dotenv").config();
+app.use(express.static("public"));
+
 app.use(helmet());
-app.use(limiter);
-app.use(logger(formatsLogger));
+// app.use(limiter);
+// app.use(logger(formatsLogger));
+app.get("env") !== "test" && app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: 15000 }));
 app.use(boolParser());
 
 app.use("/api/users", usersRouter);
